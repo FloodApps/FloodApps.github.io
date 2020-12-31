7e2c1c5cb5c9a5ba461e6479c0cb266eceab954f
BuildTools <unconfigured@null.spigotmc.org>
2020-12-31 18:42:55 -0300

    actualizacion

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,20 @@
 let myImage = document.querySelector('img');
 let miBoton = document.querySelector('button');
 let miTitulo = document.querySelector('h1');
+let imagen = document.getElementById("DImagen");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute('src');
   if (mySrc === 'images/gatito.jpg') {
     myImage.setAttribute('src', 'images/perrito.jpg');
     miTitulo.textContent = "Perrito Lindo";
+    imagen.setAttribute("href", "images/perrito.jpg");
+    imagen.setAttribute("download", "perrito.jpg");
   } else {
     myImage.setAttribute('src', 'images/gatito.jpg');
     miTitulo.textContent = "Gatito Lindo";
+    imagen.setAttribute("href","images/gatito.jpg");
+    imagen.setAttribute("download","gatito.jpg");
   }
 };
 
@@ -19,7 +24,7 @@ function estableceNombreUsuario() {
     estableceNombreUsuario();
   } else {
     localStorage.setItem('nombre', miNombre);
-    miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+    miTitulo.innerHTML = 'Buen día, ' + miNombre;
   }
 }
 
@@ -28,7 +33,7 @@ if (!localStorage.getItem('nombre')) {
 }
 else {
   let nombreAlmacenado = localStorage.getItem('nombre');
-  miTitulo.textContent = 'Buen dia, ' + nombreAlmacenado;
+  miTitulo.textContent = 'Buen día, ' + nombreAlmacenado;
 };
 
 miBoton.onclick = function () {
