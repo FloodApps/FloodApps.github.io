79c5bfcccccb6ecec8b7f7d95fc514b4ed8fb144
BuildTools <unconfigured@null.spigotmc.org>
2021-01-02 17:17:28 -0300

    actualizacion

--- scripts/main.js
+++ scripts/main.js
@@ -13,14 +13,14 @@ myImage.onclick = function () {
   } else {
     myImage.setAttribute('src', 'images/gatito.jpg');
     miTitulo.textContent = "Gatito Lindo";
-    imagen.setAttribute("href","images/gatito.jpg");
-    imagen.setAttribute("download","gatito.jpg");
+    imagen.setAttribute("href", "images/gatito.jpg");
+    imagen.setAttribute("download", "gatito.jpg");
   }
 };
 
 function estableceNombreUsuario() {
   let miNombre = prompt('Introduzca su nombre.');
-  if(!miNombre) {
+  if (!miNombre) {
     estableceNombreUsuario();
   } else {
     localStorage.setItem('nombre', miNombre);
@@ -39,3 +39,12 @@ else {
 miBoton.onclick = function () {
   estableceNombreUsuario();
 };
+
+window.onload = function () {
+
+let videoYT = document.getElementById("videoYT")
+
+videoYT.src = "https://www.youtube.com/embed/gxE4ngu78Ro";
+document.getElementById("maps").src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13135.413946342771!2d-58.38310044999999!3d-34.6078662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1609616675024!5m2!1ses!2sar"
+
+}
