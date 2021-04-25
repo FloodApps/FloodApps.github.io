790d435eb83f6fa98c406e1c8a2f368c1227748f
Patricio <pflood@frba.utn.edu.ar>
2021-04-24 21:21:54 -0300

    Nueva Actualizacion

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,95 @@
-let myImage = document.querySelector('img');
-let miBoton = document.querySelector('button');
-let miTitulo = document.querySelector('h1');
-let imagen = document.getElementById("DImagen");
+const app = new Vue({
+  el: ".cuerpo",
+  data: {
+    errorNombre: "",
+    nombre: "",
+    email: "",
+    nameValid: false,
+    errorEmail: "",
+    emailValid: false,
+    mensaje: "",
+    errorMensaje: "",
+    mensajeValid: false,
+  },
+  methods:{
 
-myImage.onclick = function () {
-  let mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/gatito.jpg') {
-    myImage.setAttribute('src', 'images/perrito.jpg');
-    miTitulo.textContent = "Perrito Lindo";
-    imagen.setAttribute("href", "images/perrito.jpg");
-    imagen.setAttribute("download", "perrito.jpg");
-  } else {
-    myImage.setAttribute('src', 'images/gatito.jpg');
-    miTitulo.textContent = "Gatito Lindo";
-    imagen.setAttribute("href", "images/gatito.jpg");
-    imagen.setAttribute("download", "gatito.jpg");
+    verifyName(){
+      if(this.nombre === ""){
+        this.errorNombre = "El nombre es necesario";
+      }
+      else if(this.nombre.length < 3){
+        this.errorNombre = "Nombre muy corto";
+      } else {
+        this.errorNombre = "";
+      }
+      this.nameValid = !this.errorNombre;
+    },
+    resetErrorName(){
+      this.nameValid = false;
+      this.errorNombre = "";
+    },
+
+    verifyEmail(){
+      if(this.email === ""){
+        this.errorEmail = "El email es necesario";
+      } else if (!this.validEmail(this.email)){
+        this.errorEmail = "El email es incorrecto";
+      } else {
+        this.errorEmail = "";
+      }
+      this.emailValid = !this.errorEmail;
+    },
+    validEmail: function (email) {
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+    resetErrorEmail(){
+      this.emailValid = false;
+      this.errorEmail = "";
+    },
+
+    verifyMensaje(){
+      if(this.mensaje === ""){
+        this.errorMensaje = "Debe escribir un mensaje"
+      } else if (this.mensaje.length < 20) {
+        this.errorMensaje = "El mensaje debe contener un mínimo de 20 carácteres"
+      } else {
+        this.errorMensaje = ""
+      }
+      this.mensajeValid = !this.errorMensaje;
+    },
+
+    resetErrorMensaje(){
+      this.mensajeValid = false;
+      this.errorMensaje = "";
+    },
+
+    enviarFormulario(){
+      this.verifyEmail();
+      this.verifyName();
+      this.verifyMensaje();
+      if(!this.nameError && !this.emailError && !this.mensajeError){
+        this.email = "";
+        this.nombre = "";
+        this.mensaje = "";
+        this.emailValid = false;
+        this.nameValid = false;
+        this.mensajeValid = false;
+        alert("Formulario Enviado");
+      }
+    },
+
+  },
+  computed: {
+    nameError: function(){
+      return this.errorNombre !== "";
+    },
+    emailError: function(){
+      return this.errorEmail !== "";
+    },
+    mensajeError: function(){
+      return this.errorMensaje !== "";
+    },
   }
-};
-
-function estableceNombreUsuario() {
-  let miNombre = prompt('Introduzca su nombre.');
-  if (!miNombre) {
-    if(localStorage.getItem("nombre")===null){
-      estableceNombreUsuario();
-    } 
-  } else {
-    localStorage.setItem('nombre', miNombre);
-    miTitulo.innerHTML = 'Buen día, ' + miNombre;
-  }
 }
-
-if (!localStorage.getItem('nombre')) {
-  estableceNombreUsuario();
-}
-else {
-  let nombreAlmacenado = localStorage.getItem('nombre');
-  miTitulo.textContent = 'Buen día, ' + nombreAlmacenado;
-};
-
-miBoton.onclick = function () {
-  estableceNombreUsuario();
-};
-
-window.onload = function () {
-
-let videoYT = document.getElementById("videoYT")
-
-videoYT.src = "https://www.youtube.com/embed/gxE4ngu78Ro";
-document.getElementById("maps").src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13135.413946342771!2d-58.38310044999999!3d-34.6078662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1609616675024!5m2!1ses!2sar"
-
-}
+)
